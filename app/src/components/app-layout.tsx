8efe434f733363c8fa01f88191522da77c485ff8
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-04-03 13:00:30 +0700

    fix gap app-layout for chilren component

--- app/src/components/app-layout.tsx
+++ app/src/components/app-layout.tsx
@@ -22,7 +22,7 @@ export function AppLayout() {
         <SiteHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+            <div className="flex flex-col gap-4 p-4 md:gap-6 md:p-6">
               <Outlet />
             </div>
           </div>
