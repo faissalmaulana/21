f6a648fbebb3aef4870d50ef94f1a9cd1a7f8cea
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-04-03 12:43:24 +0700

    reroute

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,22 +1,23 @@
 import { Project, Projects, Root, Task, Tasks } from "@/pages"
 import { BrowserRouter, Routes, Route } from "react-router"
+import { AppLayout } from "@/components/app-layout"
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route>
+        <Route element={<AppLayout />}>
           <Route index element={<Root />} />
-        </Route>
 
-        <Route path="tasks">
-          <Route index element={<Tasks />} />
-          <Route path=":id" element={<Task />} />
-        </Route>
+          <Route path="tasks">
+            <Route index element={<Tasks />} />
+            <Route path=":id" element={<Task />} />
+          </Route>
 
-        <Route path="projects">
-          <Route index element={<Projects />} />
-          <Route path=":id" element={<Project />} />
+          <Route path="projects">
+            <Route index element={<Projects />} />
+            <Route path=":id" element={<Project />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
