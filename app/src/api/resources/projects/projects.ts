cc48a6c7beba03845db2fc93a5ebde7726e617f6
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-04-04 13:26:13 +0700

    implement params builder for filtering

--- app/src/api/resources/projects/projects.ts
+++ app/src/api/resources/projects/projects.ts
@@ -1,37 +1,43 @@
-import type { Pagination, RawResponse } from "@/api/response";
-import type { Project, GetProjectsParams } from "@/api/resources";
-import { AppError } from "@/lib/app-error";
+import type { Pagination, RawResponse } from "@/api/response"
+import type { Project, GetProjectsParams } from "@/api/resources"
+import { AppError } from "@/lib/app-error"
 
 const PROJECTS_KEY = "projects"
 
-/**
- * @todo
- * - use opt parameters for filtering
- */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-async function getProjects(opt: GetProjectsParams): Promise<{ projects: Project[]; pagination: Pagination | null; }> {
-  const response = await fetch('/dummies/projects.json');
-  if (!response.ok) {
-    throw new AppError({
-      status: 500,
-      message: "Internal Server Error"
-    });
+async function getProjects(
+  opt: GetProjectsParams
+): Promise<{ projects: Project[]; pagination: Pagination | null }> {
+  const url = new URL('/dummies/projects.json', window.location.origin)
+
+  // build query params
+  if (opt.search) {
+    url.searchParams.set("search", opt.search)
   }
 
-  const result: RawResponse<{ projects: Project[]; pagination: Pagination | null; }> = await response.json();
+  const response = await fetch(url.toString())
 
+  const result: RawResponse<{
+    projects: Project[]
+    pagination: Pagination | null
+  }> = await response.json()
+
+  // API-level error handling
   switch (result.status) {
     case 200:
-      return result.data;
+      return result.data
+
+    case 400:
+      throw new AppError({
+        status: 400,
+        message: result?.error?.message ?? "Bad Request",
+      })
+
     default:
       throw new AppError({
-        status: 500,
-        message: "Internal Server Error"
-      });
+        status: result.status ?? 500,
+        message: result?.error?.message ?? "Internal Server Error",
+      })
   }
 }
 
-export {
-  PROJECTS_KEY,
-  getProjects
-}
+export { PROJECTS_KEY, getProjects }
