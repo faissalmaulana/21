40e62a2d1792d3b3679264678fd288824afedb9f
faissalmaulana <114222238+faissalmaulana@users.noreply.github.com>
2026-04-09 17:12:22 +0700

    add sonner component

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -5,12 +5,14 @@ import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Toaster } from "@/components/ui/sonner.tsx"
 
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
+      <Toaster />
       <TooltipProvider>
         <QueryClientProvider client={queryClient}>
           <App />
